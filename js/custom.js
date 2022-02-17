f925cbd84a2f04144cddbd5706117a52b069dc05
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-02-18 00:57:25 +0600

    just

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    "use strict";
+
+    $(".banner-slider").slick({
+        autoplay: false ,
+        dots: false,
+        prevArrow:'<span class="left"><i class="fas fa-chevron-left leftarrow"></i></span>',
+        nextArrow:'<span class="right"><i class="fas fa-chevron-right rightarrow"></i></span>',
+    });
+    
+})
