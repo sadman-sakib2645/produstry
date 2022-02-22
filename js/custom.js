8bb69018f07f5e82d7b3c47d8a91e17cd97115f3
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-02-23 02:02:59 +0600

    cheak

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,54 @@ $(document).ready(function(){
 
     $(".banner-slider").slick({
         autoplay: false ,
-        dots: false,
-        prevArrow:'<span class="left"><i class="fas fa-chevron-left leftarrow"></i></span>',
-        nextArrow:'<span class="right"><i class="fas fa-chevron-right rightarrow"></i></span>',
+        dots: true,
+        prevArrow:'.leftarrow',
+        nextArrow:'.rightarrow',
     });
+
+    $('.counter').counterUp({
+        delay: 50,
+        time: 5000
+    });
+
+    new VenoBox({
+        selector: '.venobox'
+    });
+    $(".blog-slider").slick({
+        arrows:true,
+        autoplay: true ,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll:1,
+        centerMode: true,
+        centerPadding: '0px',
+        prevArrow:'.blog-leftarrow',
+        nextArrow:'.blog-rightarrow',
+    });
+
+    $(".testi-slider").slick({
+        arrows:false,
+        autoplay: true ,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll:1,
+        // asNavFor: '.slider-nav',
+        // prevArrow:'.blog-leftarrow',
+        // nextArrow:'.blog-rightarrow',
+    });
+    $(".testi-main").slick({
+        arrows:false,
+        autoplay: true ,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll:1,
+        centerMode: true,
+        centerPadding: '0px',
+        // asNavFor: '.slider-for',
+        // prevArrow:'.blog-leftarrow',
+        // nextArrow:'.blog-rightarrow',
+    });
+
+    
     
 })
