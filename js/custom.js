915339d1be001871c446442e49b49a2cf33ac0a6
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-02-28 16:26:33 +0600

    chk

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
         dots: true,
         prevArrow:'.leftarrow',
         nextArrow:'.rightarrow',
+        
     });
 
     $('.counter').counterUp({
@@ -18,7 +19,7 @@ $(document).ready(function(){
     });
     $(".blog-slider").slick({
         arrows:true,
-        autoplay: true ,
+        autoplay: false ,
         dots: false,
         slidesToShow: 3,
         slidesToScroll:1,
@@ -26,31 +27,88 @@ $(document).ready(function(){
         centerPadding: '0px',
         prevArrow:'.blog-leftarrow',
         nextArrow:'.blog-rightarrow',
+        responsive:[
+            {
+                breakpoint: 575,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                
+                }
+            }
+        ]
     });
 
     $(".testi-slider").slick({
-        arrows:false,
+        arrows:true,
         autoplay: true ,
-        dots: false,
         slidesToShow: 1,
         slidesToScroll:1,
-        // asNavFor: '.slider-nav',
-        // prevArrow:'.blog-leftarrow',
-        // nextArrow:'.blog-rightarrow',
+        asNavFor: '.testi-main',
+        draggble:false ,
+        prevArrow:'.testi-leftarrow',
+        nextArrow:'.testi-rightarrow',
     });
     $(".testi-main").slick({
         arrows:false,
         autoplay: true ,
-        dots: false,
         slidesToShow: 3,
         slidesToScroll:1,
         centerMode: true,
         centerPadding: '0px',
-        // asNavFor: '.slider-for',
-        // prevArrow:'.blog-leftarrow',
-        // nextArrow:'.blog-rightarrow',
+        asNavFor: '.testi-slider',
+        focusOnSelect:true,
     });
 
-    
+    $(".brand-slider").slick({
+        arrows:true,
+        autoplay: true ,
+        dots: false,
+        slidesToShow: 5,
+        slidesToScroll:1,
+        centerMode: true,
+        centerPadding: '0px',
+        prevArrow:'.brand-leftarrow',
+        nextArrow:'.brand-rightarrow',
+        responsive:[
+            {
+                breakpoint: 575,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                
+                }
+            }
+        ]
+    });
     
 })
